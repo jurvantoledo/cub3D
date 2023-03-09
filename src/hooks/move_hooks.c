03a58044b1d4e64ecd71dcbc822473d68b7729fc
Jur Van toledo <jvan-tol@f1r6s13.codam.nl>
2023-03-09 15:48:27 +0100

    added movement

--- src/hooks/move_hooks.c
+++ src/hooks/move_hooks.c
@@ -6,7 +6,7 @@
 /*   By: jvan-tol <jvan-tol@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/08 14:30:04 by jvan-tol      #+#    #+#                 */
-/*   Updated: 2023/03/09 12:54:46 by jvan-tol      ########   odam.nl         */
+/*   Updated: 2023/03/09 15:43:53 by jvan-tol      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,54 @@
 
 static void	move_forward(t_data *data, t_player *player)
 {
-	double	new_x;
-	double	new_y;
+	double	x;
+	double	y;
 
-	player->loc.x += player->direction.x * data->move_speed;
-	player->loc.y += player->direction.y * data->move_speed;
+	x = player->loc.x + player->direction.x * data->move_speed;
+	y = player->loc.y + player->direction.y * data->move_speed;
+	if (data->map.world_map[(int)floor(x)][(int)floor(player->loc.y)] != '1')
+		player->loc.x += player->direction.x * data->move_speed;
+	if (data->map.world_map[(int)floor(player->loc.x)][(int)floor(y)] != '1')
+		player->loc.y += player->direction.y * data->move_speed;
 }
 
 static void	move_back(t_data *data, t_player *player)
 {
-	double	new_x;
-	double	new_y;
+	double	x;
+	double	y;
 
-	player->loc.x -= player->direction.x * data->move_speed;
-	player->loc.y -= player->direction.y * data->move_speed;
+	x = player->loc.x - player->direction.x * data->move_speed;
+	y = player->loc.y - player->direction.y * data->move_speed;
+	if (data->map.world_map[(int)floor(x)][(int)floor(player->loc.y)] != '1')
+		player->loc.x -= player->direction.x * data->move_speed;
+	if (data->map.world_map[(int)floor(player->loc.x)][(int)floor(y)] != '1')
+		player->loc.y -= player->direction.y * data->move_speed;
 }
 
 static void	move_left(t_data *data, t_player *player)
 {
-	player->loc.x += player->direction.y * data->move_speed;
-	player->loc.y += -player->direction.x * data->move_speed;
+	double	x;
+	double	y;
+
+	x = player->loc.x + player->direction.y * data->move_speed;
+	y = player->loc.y + -player->direction.x * data->move_speed;
+	if (data->map.world_map[(int)floor(x)][(int)floor(player->loc.y)] != '1')
+		player->loc.x += player->direction.y * data->move_speed;
+	if (data->map.world_map[(int)floor(player->loc.x)][(int)floor(y)] != '1')
+		player->loc.y += -player->direction.x * data->move_speed;
 }
 
 static void	move_right(t_data *data, t_player *player)
 {
-	player->loc.x += -player->direction.y * data->move_speed;
-	player->loc.y += player->direction.x * data->move_speed;
+	double	x;
+	double	y;
+
+	x = player->loc.x + -player->direction.y * data->move_speed;
+	y = player->loc.y + player->direction.x * data->move_speed;
+	if (data->map.world_map[(int)floor(x)][(int)floor(player->loc.y)] != '1')
+		player->loc.x += -player->direction.y * data->move_speed;
+	if (data->map.world_map[(int)floor(player->loc.x)][(int)floor(y)] != '1')
+		player->loc.y += player->direction.x * data->move_speed;
 }
 
 void	exec_moves(t_data *data, t_player *player)
